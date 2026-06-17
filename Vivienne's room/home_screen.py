323ba9fbb7950a59b7d86cb7edb2f0f4fcb98a7c
Vivienne Cleary <vivienne.cleary@ericssons.com>
2026-06-17 13:47:02 +0100

    made win page

--- Vivienne's room/home_screen.py
+++ Vivienne's room/home_screen.py
@@ -15,22 +15,38 @@ color = (225, 225, 225) #white
 fullscreen = False
 running = True
 
+#switching betweeen pages
+HOME = 0
+MAIN_MENU = 1
+SETTING = 2
+
+game_state = HOME
+
 #import
 text1 = font1.render("Welcome to Jarry's House", True, color)
 text2 = font2.render("Click anywhere to begin", True, color)
+Home_image = pygame.image.load(join("images", "jarrys_house.png")).convert_alpha() 
 
 #centre buttons
 screen_width = screen.get_width()
 center_x = screen_width // 2
 
-Text_rect1= text1.get_rect(center=(center_x, 250))
-Text_rect2 = text2.get_rect(center=(center_x, 350))
+Text_rect1= text1.get_rect(center=(center_x, 350))
+Text_rect2 = text2.get_rect(center=(center_x, 450))
+Home_image_rect = Home_image.get_rect()
+Home_image_rect.left = 5     # 20 pixels from left edge
+Home_image_rect.centery = screen.get_height() // 2
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+
+        if game_state == HOME:
+       
+        elif game_state == MAIN_MENU:
+            ..
+            
     text1 = font1.render("Welcome to Jarry's House", True, color)
     text2 = font2.render("Click anywhere to begin", True, color)
 
@@ -38,5 +54,6 @@ while running:
     screen.fill((30, 30, 30))
     screen.blit(text1,Text_rect1) 
     screen.blit(text2,Text_rect2) 
+    screen.blit(Home_image, Home_image_rect)
     pygame.display.update()
 pygame.quit()
